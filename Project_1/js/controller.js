aaea0aedc6a6799b03a6259e51d1b7dbdbf1163f
Valerio <valerio.zocco@gmail.com>
2025-02-23 15:29:58 +0100

    Scrittura del controller per la comunicazione tra modello e view

---
+++ Project_1/js/controller.js
@@ -0,0 +1,37 @@
+const TaskController = (function (model, view) {
+    function init() {
+        view.init();
+        view.renderTasks(model.tasks);
+        view.bindAddTask(handleAddTask);
+        view.bindDeleteTask(handleDeleteTask);
+        view.bindToggleTask(handleToggleTask);
+    }
+
+    function handleAddTask() {
+        const title = view.getTaskInput().trim();
+        if (taskTitle === '') return;
+        const task = {
+            id: Date.now().toString(),
+            title: taskTitle,
+            completed: false
+        };
+        model.addTask(task);
+        view.clearTaskInput();
+        view.renderTasks(model.getTasks());
+    }
+
+    function handleDeleteTask(id) {
+        model.removeTask(id);
+        view.renderTasks(model.getTasks());
+    }
+
+    function handleToggleTask(id) {
+        model.toggleTask(id);
+        view.renderTasks(model.getTasks());
+    }
+
+    return{
+        init
+    };
+
+})(TaskModel, TaskView);
